8ba51516133ed038be8a08c82f11249387956753
Maahi Patel <maahi.patel@1Rintern.local>
2023-03-22 19:46:12 +0530

    forms

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -1,10 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'routing';
+
+  public limit:number=5;
+  public totalbtn:any;
+  public btnArr:number[]=[]
+  public findex:any;
+  public lindex:any;
+  public newData:any;
+  public data = [
+    {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:2,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"virghjghhgen"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"viren"
+  }
+  ,
+  {
+    id:1,
+    name:"viren"
+  },
+  {
+    id:1,
+    name:"vcbcvbcviren"
+  },
+] 
+ngOnInit(): void {
+    this.totalbtn = this.data.length/this.limit;
+   for (let index = 1; index <= Math.ceil( this.totalbtn); index++) {
+    this.btnArr.push(index)
+    console.log(this.btnArr); 
+    this.newData = this.data.filter(i=>this.data.indexOf(i)<=this.limit-1);  
+   }    
+}
+showData(data:any){
+
+this.findex = (data.target.value-1)*this.limit;
+this.lindex = this.findex + this.limit - 1;
+console.log(this.findex,this.lindex);
+this.newData = this.data.filter(i=>this.data.indexOf(i)>=this.findex && this.data.indexOf(i)<=this.lindex);
+}
+
 }
