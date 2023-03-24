e5438041a4346d790aaeb33590797954facfe489
Maahi Patel <maahi.patel@1Rintern.local>
2023-03-24 17:59:29 +0530

    file added

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -9,9 +9,17 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'template-driven-forms';
   topics=['Angular','React','Vue'];
+  form: NgForm | undefined;
   
-  onSubmit(form:NgForm){
-console.log(form);
-
+   onSubmit(form:NgForm){
+ console.log(form);
+   }
+  setDefaultValues(){
+     this.form?.form.patchValue({
+      conditions:true,
+      email:'abc@gmail.com',
+      name:'maahi',
+      topics:'Angular'
+     })
   }
 }
